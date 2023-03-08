8c2700b7a639f1c38166769f865bb879796c787d
woonhk90 <wonho7344@gmail.com>
2023-03-08 22:11:30 +0900

    사용자 정의 컨텍스트 제공자 구성요소 빌드 및 사용

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {}, //TIP ( 기본컨텍스트에 추가하면 IDE자동완성으로 찾아볼 수 있다. 값은 그냥 더미(아무거도 사용하지 않는) 값(함수)을 지정함 )
 });
 
 export default AuthContext;
